dd5181f29399c8b0d558047b6c039adbe676c0b7
o3etornam <03etornam@gmail.com>
2024-05-29 16:07:03 +0000

    added an html file to simulate submitting a form

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI, UploadFile
 
+
+
 app = FastAPI()
 
-@app.post("/uploadfile/")
+@app.post("/uploadfile")
 async def create_upload_file(file: UploadFile):
     return {"filename": file.filename}
