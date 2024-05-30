b035402a8df2220372c4d5cac5b5decdf472b5f6
Frederick Otu-Afro <frederickotuafro@gmail.com>
2024-05-30 13:40:43 +0000

    changes made, including API Keys

--- perp.js
+++ perp.js
@@ -27,3 +27,37 @@ document.getElementById('refreshButton').addEventListener('click', function() {
     questionInput.value = '';
   });
 
+
+
+//This part was written by Frederick
+const API_KEY = 'sk-proj-t0Uu83FTGrdB42sXgHk4T3BlbkFJtsijqDutQOkuXUjB6AFN'
+const submitButton = document.querySelector('#askButton')
+
+
+
+async function getMessage() {
+    //console.log('clicked')
+    const options = {
+        method: 'POST',
+        headers: {
+            'Authorization': 'Bearer ${API_KEY}',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            model: "gpt-3.5-turbo-16k",
+            messages: [{role: "user", content: "Hello!"}],
+            max_tokens: 100
+            })  
+    }
+    try {
+        const response = await fetch('https://api.openai.com/v1/chat/completions', options)
+        const data = await response.json()
+        console.log(data)
+    }
+    catch (error){
+        console.error(error)
+    }
+}
+
+submitButton.addEventListener('click', getMessage)
+
