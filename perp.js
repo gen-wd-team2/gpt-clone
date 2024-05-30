88c8a10baab88e7212a05129cffc3333d5da9683
Frederick Otu-Afro <frederickotuafro@gmail.com>
2024-05-30 14:14:28 +0000

    font changed to Open Sans

--- perp.js
+++ perp.js
@@ -36,27 +36,27 @@ const submitButton = document.querySelector('#askButton')
 
 
 async function getMessage() {
-    //console.log('clicked')
-    const options = {
-        method: 'POST',
-        headers: {
-            'Authorization': 'Bearer ${API_KEY}',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            model: "gpt-3.5-turbo-16k",
-            messages: [{role: "user", content: "Hello!"}],
-            max_tokens: 100
-            })  
-    }
-    try {
-        const response = await fetch('https://api.openai.com/v1/chat/completions', options)
-        const data = await response.json()
-        console.log(data)
-    }
-    catch (error){
-        console.error(error)
-    }
+    console.log('clicked')
+    // const options = {
+    //     method: 'POST',
+    //     headers: {
+    //         'Authorization': 'Bearer ${API_KEY}',
+    //         'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify({
+    //         model: "gpt-3.5-turbo-16k",
+    //         messages: [{role: "user", content: "Hello!"}],
+    //         max_tokens: 100
+    //         })  
+    // }
+    // try {
+    //     const response = await fetch('https://api.openai.com/v1/chat/completions', options)
+    //     const data = await response.json()
+    //     console.log(data)
+    // }
+    // catch (error){
+    //     console.error(error)
+    // }
 }
 
 submitButton.addEventListener('click', getMessage)
